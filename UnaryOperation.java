fe05504084a29abf4cb9cd1d3f8b1cf95b7bb56d
Radheshyamkumar2611 <139638042+Radheshyamkumar2611@users.noreply.github.com>
2023-10-15 09:30:54 +0530

    new

---
+++ UnaryOperation.java
@@ -0,0 +1,9 @@
+public class UnaryOperation {
+    public static void main(String args[]) {
+        int a = 10;
+        int b = a++;
+        // (b = a+1);
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
